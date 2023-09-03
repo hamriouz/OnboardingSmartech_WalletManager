0397daed778d54d646b03726c65ddde0192f56bb
Hamraz <hamriouz@gmail.com>
2023-09-03 14:27:02 +0330

    Make all needed changes for kafka producer and consumer.

---
+++ src/main/kotlin/com/example/WalletManager/kafka/KafkaConsumer.kt
@@ -0,0 +1,33 @@
+package com.example.WalletManager.kafka
+
+import com.example.WalletManager.model.Transaction
+import com.example.WalletManager.model.TransactionType
+import com.example.WalletManager.service.UserServiceImp
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+
+@Service
+class KafkaConsumer(
+    private var userServiceImp: UserServiceImp
+    ) {
+    @KafkaListener(topics = [AppConstants.TOPIC_NAME])
+    fun listen(msg:String){
+        print("Message Received - $msg")
+        val col = ":"
+        val equal = "="
+        val list = msg.split(col)
+        val userId = list[0].split(equal)[1].toInt()
+        val amount = list[1].split(equal)[1].toInt()
+        val transactionType = list[2].split(equal)[1]
+
+        userServiceImp.makeTransaction(Transaction(getTransactionType(transactionType), userId, amount))
+
+    }
+    private fun getTransactionType(type: String): TransactionType {
+        return if (type == "DECREASE") {
+            TransactionType.DECREASE
+        } else TransactionType.INCREASE
+
+    }
+
+}
--- src/main/kotlin/com/example/WalletManager/service/UserServiceImp.kt
+++ src/main/kotlin/com/example/WalletManager/service/UserServiceImp.kt
@@ -1,16 +1,17 @@
 package com.example.WalletManager.service
 
 import com.example.WalletManager.UserService
+import com.example.WalletManager.kafka.KafkaProducer
 import com.example.WalletManager.model.*
 import com.example.WalletManager.repository.UserRepository
 import com.example.WalletManager.repository.WalletRepository
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 class UserServiceImp(
-    private var userDB: UserRepository,
-    private var walletDB: WalletRepository,
+    private val userDB: UserRepository,
+    private val walletDB: WalletRepository,
+    private val kafkaProducer: KafkaProducer,
 ) : UserService{
 
     override fun getUserById(id: Int): FullUser {
@@ -22,14 +23,36 @@ class UserServiceImp(
     override fun createNewUser(userName: SetOrChangeName): String {
         val user = userDB.save(User(name = userName.name))
         val userId = user.id
-        val wallet = walletDB.save(Wallet(userId, 0))
+        walletDB.save(Wallet(userId, 0))
         return "User created with id $userId"
     }
 
-    override fun transaction(transaction: Transaction): Transaction {
-        val user = userDB.findById(transaction.userId)
+    override fun transaction(transaction: Transaction): String {
+        val kafkaMessage = createKafkaMessage(transaction)
+        kafkaProducer.sendEvent(kafkaMessage)
+        return "The transaction was successfully added to the transaction queue"
+    }
+
+    private fun createKafkaMessage(transaction: Transaction): String {
+        val userid = transaction.userId
+        val amount = transaction.amount
+        val transactionType = transaction.transactionType
+        return "userid=$userid:amount=$amount:transactionType=$transactionType"
+    }
+
+    fun makeTransaction(transaction: Transaction): TransactionStatus {
         val wallet = walletDB.findByUserId(transaction.userId)
-        return transaction
+        if (transaction.transactionType == TransactionType.DECREASE) {
+            if (wallet!!.balance!! < transaction.amount) {
+                return TransactionStatus.NOT_ENOUGH_BALANCE
+            }
+            wallet.balance  = wallet.balance?.minus(transaction.amount)
+            walletDB.save(wallet)
+            return TransactionStatus.SUCCESSFUL
+        }
+        wallet!!.balance = wallet.balance?.plus(transaction.amount)
+        walletDB.save(wallet)
+        return TransactionStatus.SUCCESSFUL
     }
 
     override fun renameUser(newName: SetOrChangeName, id: Int): String {
@@ -39,7 +62,4 @@ class UserServiceImp(
         val name = newName.name
         return "User's name with userid $id was successfully changed to $name"
     }
-
-
 }
-
